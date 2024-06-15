ee2d48583fb55d6ce1d584428ba3513291f9f981
Shadow Valentine <32857579+shadowvalentine@users.noreply.github.com>
2024-06-15 20:16:40 +0800

    Add github actions

---
+++ .github/actions/builder.sh
@@ -0,0 +1,9 @@
+
+if [ ! -f /rathena/make ]; then
+  echo "Warning: ./configure will be executed with provided values";
+  echo "Make sure you have set the variables you want in the docker-compose.yml file";
+  echo $BUILDER_CONFIGURE
+  ./configure $BUILDER_CONFIGURE
+fi
+
+make clean server;
